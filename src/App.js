491e5067fdbeabdbe1337ec498b0cb4d0e63fd32
Luja Chitrakar <122864496+LujaChitrakar@users.noreply.github.com>
2024-03-09 21:42:10 +0545

    Added files
    Added all the files that were coded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [diceCount, setDiceCount] = useState("");
+  const [diceType, setDiceType] = useState(4);
+  const [result, setResult] = useState([]);
+
+  const dice = (side) => {
+    return Math.floor(Math.random() * side) + 1;
+  };
+  const count = diceCount.valueOf();
+  const type = diceType.valueOf();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (count && type) {
+      const spin = [];
+      for (let i = 0; i < parseInt(count); i += 1) {
+        const Roll = dice(parseInt(type));
+        spin.push(Roll);
+      }
+      setResult(spin);
+      console.log(result);
+    } else {
+      console.log("error");
+    }
+  };
+  useEffect(() => {
+    console.log(result);
+  }, [result]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="diceRoll">
+      <section>
+        <form onSubmit={handleSubmit} className="diceForm">
+          <h1>Roll the dice</h1>
+          <div className="inputs">
+            <div className="inputType">
+              <label>How many dices to roll</label>
+              <input
+                value={diceCount}
+                onChange={(e) => {
+                  setDiceCount(e.target.value);
+                }}
+                name="dice-count"
+                id="dice-count"
+              />
+            </div>
+            <br />
+            <div className="inputType">
+              <label>Choose the number of sides</label>
+              <select
+                name="dice-type"
+                id="dice-type"
+                value={diceType}
+                onChange={(e) => {
+                  setDiceType(e.target.value);
+                }}
+              >
+                <option value="4">D4</option>
+                <option value="6">D6</option>
+                <option value="8">D8</option>
+                <option value="12">D12</option>
+              </select>
+            </div>
+          </div>
+          <button id="submit" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section id="result">
+        {result.map((diceResult, index) => (
+          <div key={index} className="dices">
+            {diceResult}
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
